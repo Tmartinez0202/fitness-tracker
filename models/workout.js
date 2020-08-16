3c431078965675ebceeba2fe56cb9be5d3b4139c
OkieKenobi <travismartinez88@gmail.comgit config --global user.name Tmartinez0202>
2020-08-16 13:26:04 -0600

    added models file with index and workout and added the exercise models

---
+++ models/workout.js
@@ -0,0 +1,59 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema (
+    {
+        day: {
+                type: Date,
+                default: () => new Date ()
+            },
+
+        exercises: [ {
+                type: {
+                        type: String,
+                        trim: true,
+                        required: "Pick an exercise type"
+                        },
+                name: {
+                        type: String,
+                        trim: true,
+                        required: "Enter exercise name"
+                        },
+                duration: {
+                        type: Number,
+                        required: "Enter length of workout"
+                        },
+                weight: {
+                        type: Number
+                        },
+                sets: {
+                        type: Number
+                         },
+                reps: {
+                        type: Number
+                        },
+                duration: {
+                        type: Number
+                        }
+                    
+                    } 
+                ]
+            },
+                {    
+                toJSON: {
+                        virtuals: true
+                        }
+        
+                }
+        
+            )
+
+
+            workoutSchema.virtual("totalDuration").get(function() {
+                return this.exercises.reduce((total, exercise) => {
+                    return total + exercise.duration
+                }, 0)
+            })
+
+            const Workout = mongoose.model("workoutdb", workoutSchema)
+            module.exports = Workout
